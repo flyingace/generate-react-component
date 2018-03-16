8b0777623b51552121feace0343e7cb7798b0fda
flyingace <dc@davidcameron.biz>
2018-03-16 16:32:40 -0400

    Added a template for stories

---
+++ generators/app/templates/component-stateful.stories.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { backgrounds } from '../../../stories/addons/backgrounds';
+import <%= componentName %>  from './<%= componentName %>';
+
+storiesOf('<%= componentName %>', module)
+  .addDecorator(backgrounds)
+  .add('<%= componentName %>', () => {
+    return (<<%= componentName %> />);
+});
