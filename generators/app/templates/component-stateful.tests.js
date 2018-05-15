f298f3e28c42cfc38ff7f40edb16597c001f1cbf
flyingace <dc@davidcameron.biz>
2018-05-15 10:42:41 -0400

    Added comments with guidelines about what tests should check

--- generators/app/templates/component-stateful.tests.js
+++ generators/app/templates/component-stateful.tests.js
@@ -5,7 +5,16 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import <%= componentName %> from '../<%= componentName %>';
 
+/*
+What to Test?
+In comments are some general guidelines for what to test in a React component.
+These came from Getting Started with TDD in React:
+https://daveceddia.com/getting-started-with-tdd-in-react/
+*/
+
 describe('<%= componentName %>', () => {
+
+/* Test that it renders properly */
 
   test('renders without crashing', () => {
     const Component = shallow(<<%= componentName %> />);
@@ -16,3 +25,13 @@ describe('<%= componentName %>', () => {
     expect(snapshot).toMatchSnapshot();
   });
 });
+
+/* Test the output */
+
+/* Test the states */
+
+/* Test the events */
+
+/* Test the edge cases */
+
+/* Optionally, test that no errors are printed to the console */
