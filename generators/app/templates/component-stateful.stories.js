3d7859fbad0da88ad43f63306d4d4e72b28339d9
David Cameron <dc@davidcameron.biz>
2021-06-28 17:45:39 -0400

    Hopefully I've made useful updates.

--- generators/app/templates/component-stateful.stories.js
+++ generators/app/templates/component-stateful.stories.js
@@ -1,14 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "action" }] */
-
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { backgrounds } from 'stories/addons/backgrounds';
-import <%= componentName %> from '../<%= componentName %>';
+import <%= componentName %> from './<%= componentName %>';
 
-storiesOf('<%= componentName %>', module)
-  .addDecorator(backgrounds)
-  .add('<%= componentName %>', () => {
-    return (<<%= componentName %> />);
-  });
+export default {
+  title: '<%= componentName %>',
+  component: <%= componentName %>,
+};
+
+const Template = (args) => <<%= componentName %> {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
